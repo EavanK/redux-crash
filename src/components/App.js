83d4ad8c19c4f9205a3e87fb072b78c406db2e1d
Eavan <eavan.hsk@gmail.com>
2022-02-17 01:09:25 -0800

    Setup for React-Redux

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import Detail from "../routes/Detail";
+import Home from "../routes/Home";
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Home />}></Route>
+        <Route path="/:id" element={<Detail />}></Route>
+      </Routes>
+    </Router>
+  );
+}
