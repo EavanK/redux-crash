9e6cd673a5be2fb2b56132bc817f1403c6af99e4
Eavan <eavan.hsk@gmail.com>
2022-02-17 19:25:45 -0800

    install redux/toolkit, refactor code
    -add createAction function
    -comment out constant variables for action types
    -commnet out action function definition

--- src/store.js
+++ src/store.js
@@ -1,26 +1,32 @@
 import { createStore } from "redux";
+import { createAction } from "@reduxjs/toolkit";
 
-const ADD = "ADD";
-const DELETE = "DELETE";
+// const ADD = "ADD";
+// const DELETE = "DELETE";
 
-const addToDo = (text) => {
-  return { type: ADD, text };
-};
+// const addToDo = (text) => {
+//   return { type: ADD, text };
+// };
 
-const deleteToDo = (id) => {
-  return { type: DELETE, id };
-};
+// const deleteToDo = (id) => {
+//   return { type: DELETE, id };
+// };
+
+// using createAction toolkit we can write less lines of code
+//(skip constant variables for type, action definition)
+const addToDo = createAction("ADD");
+const deleteToDo = createAction("DELETE");
 
 const storage = JSON.parse(localStorage.getItem("ToDoList")) || [];
 
 const reducer = (state = storage, action) => {
   switch (action.type) {
-    case ADD:
-      const addToDo = [{ text: action.text, id: Date.now() }, ...state];
-      localStorage.setItem("ToDoList", JSON.stringify(addToDo));
-      return addToDo;
-    case DELETE:
-      const filteredToDo = state.filter((toDo) => toDo.id !== action.id);
+    case addToDo.type:
+      const addToDos = [{ text: action.payload, id: Date.now() }, ...state];
+      localStorage.setItem("ToDoList", JSON.stringify(addToDos));
+      return addToDos;
+    case deleteToDo.type:
+      const filteredToDo = state.filter((toDo) => toDo.id !== action.payload);
       localStorage.setItem("ToDoList", JSON.stringify(filteredToDo));
       if (!storage.length) localStorage.removeItem("ToDoList");
       return filteredToDo;
